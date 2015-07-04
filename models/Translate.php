e54bd2ca4c63847b3225b4f4a33d4e7168f39c3a
infun3 <infun3@gmail.com>
2015-07-04 11:25:23 +0200

    added 5 prev translations to update action

--- models/Translate.php
+++ models/Translate.php
@@ -19,6 +19,7 @@ class Translate extends \yii\db\ActiveRecord
      * @inheritdoc
      */
 	public $source;
+	public $sources;
 	public $src;
 	public $dst;
 	public $yandex;
@@ -61,5 +62,15 @@ class Translate extends \yii\db\ActiveRecord
     public function getDirections($id)
     {
         return  (new \yii\db\Query())->select(['src','dst'])->from('translators')->where(['id' => $id])->one();
+    }
+    public function findLastXtranslations($lng, $id, $x=5)
+    {
+        Translate::setTableName($lng);
+        if (($model =Translate::find()->where(['between', 'id', $id-$x, $id])->select(['id','str'])) !== null) {
+            return $model->limit($x)->all();
+        } else {
+            //            return $this->redirect(['main']);
+            throw new NotFoundHttpException('need more vars'.PHP_EOL.$lng.PHP_EOL.$id);
+        }
     }
 }
